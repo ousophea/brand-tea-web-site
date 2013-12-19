9ab1e6135ffe54deca85378119e09ee541a0cdbb
chundany@gmail.com <chundany@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2013-12-19 01:06:26 +0000

    Administrator controller extent from core/Admin_Controller

--- qnbrandtea.com/application/modules/admin/controllers/admin.php
+++ qnbrandtea.com/application/modules/admin/controllers/admin.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Admin extends Admin_Controller {
 
 	public function index()
 	{
             $data['title']="Welcome to Tea";
-            $data['page']='dashboard';
+            $data['page']='admin/dashboard';
             $data['action']='Dashboard';
             $this->load->view('master',$data);
 	}
