bba1644ccf2284cb21b3d3bf0128783c4b5bfab0
chundany@gmail.com <chundany@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2013-12-25 17:17:04 +0000

    Front end template and static home page

---
+++ Implement/qnbrandtea.com/application/modules/site/views/home.php
@@ -0,0 +1,249 @@
+
+	<!--Start Carousel-->
+          <div id="myCarousel" class="carousel slide">
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/featured/1.jpg" alt="">
+              </div>
+              <div class="item">
+                <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/featured/2.jpg" alt="">
+              </div>
+              <div class="item">
+                <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/featured/3.jpg" alt="">
+              </div>
+            </div>
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/arrow.png" alt="Arrow"></a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/arrow2.png" alt="Arrow"></a>
+          </div>
+	<!--End Carousel-->
+    <div>  
+        <div>
+        	<div class="span5" style="padding:15px;">
+                  <h3>&gt; Welcome <span style="float: right; margin-right:25px;">Date</span></h3>
+                  <hr>
+                  
+            	<table width="100%" border="0" height="280">
+                  <tr>
+                    <td rowspan="10" class="span2">
+                    	<div style="padding-right:10px;">
+                            <p><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/welcome.png" /></p><br>
+                            <p>Lorem ipsum dolor sit amet, dolor sit amet</p>
+                        </div>
+                    </td>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                  <tr>
+                    <td>Lorem ipsum dolor sit amet</td>
+                    <td align="right">2013-12-05</td>
+                  </tr>
+                </table>
+            </div>
+            <div class="span7">
+                <div class="span4" style="padding:15px;">
+                    <h3>&gt; Gallery</h3>
+                    <hr>
+                    <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/slideshow.png" />
+                </div>
+                <div class="span2" style="padding:15px;width:220px;">
+                    <h3>Support</h3>
+                    <hr>
+                    <div class="support"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/tea.png" width="70" /> Lorem ipsum</div>
+                    <div class="support"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/tea.png" width="70" /> Lorem ipsum</div>
+                    <div class="support"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/tea.png" width="70" /> Lorem ipsum</div>
+                    <div class="support"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/tea.png" width="70" /> Lorem ipsum</div>
+                </div>
+            </div>
+        </div>  
+        
+        <div style="clear:both;padding:0 15px;">
+        <!--Start Carousel-->
+              <div id="myCarousel" class="carousel slide">
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/slide3/1.jpg" alt="">
+                  </div>
+                  <div class="item">
+                    <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/slide3/2.jpg" alt="">
+                  </div>
+                  <div class="item">
+                    <img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/slide3/3.jpg" alt="">
+                  </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/arrow.png" alt="Arrow"></a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next"><img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/arrow2.png" alt="Arrow"></a>
+              </div>
+        <!--End Carousel-->
+        </div> 
+        <div style="clear:both;padding:15px;">
+        	<div class="span7">
+            	<h3>&gt; Products</h3>
+                <hr><br>
+                <div>
+                	<div class="span7 product-row">
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    </div>
+                	<div class="span7 product-row">
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    </div>
+                	<div class="span7 product-row">
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    	<div class="product">
+                        	<img src="<?php echo base_url() . FRONTEND_TEMPLATE; ?>img/content/product.png" alt="product" width="160px">
+                            <p><a href="">/ Read More /</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        	<div class="span5" style="padding-left:20px;">
+            	<div>
+                    <h3>&gt; Welcome <span style="float: right; margin-right:25px;">Date</span></h3>
+                    <hr>
+                  
+                    <table width="100%" border="0" height="260">
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                    </table>
+                </div>
+            	<div>
+                	<br>
+                    <h3>&gt; Welcome <span style="float: right; margin-right:25px;">Date</span></h3>
+                    <hr>
+                  
+                    <table width="100%" border="0" height="260">
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                      <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td align="right">2013-12-05</td>
+                      </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
--- qnbrandtea.com/application/core/Admin_Controller.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- 
-class Admin_Controller extends MX_Controller {
- 
- 
-}
