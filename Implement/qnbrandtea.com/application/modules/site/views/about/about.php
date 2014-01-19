056c4ea55fd1e4c374a0644ee136364176842090
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-01-19 13:32:22 +0000

    site about

---
+++ Implement/qnbrandtea.com/application/modules/site/views/about/about.php
@@ -0,0 +1,28 @@
+<div style="height: 10px;"></div>
+<div class="row-fluid">
+    
+        <h3 class="page-head"><?php echo $action; ?></h3>    
+            <?php
+            echo br(1);
+            foreach ($about->result_array() as $abo) {
+                ?>   
+				  <div class="row-fluid span12">				
+                    <?php
+                     
+				     echo $abo[field('aboDesc')];
+                   
+                    ?>
+                </div>
+                <?php
+            }
+			echo br(1);
+            ?> 
+	       
+          <div class="row-fluid span12">	
+		   <h3 class="page-head"><?php echo $this->lang->line('men_follow'); ?></h3>
+		   <?php
+		   echo br(1);
+            echo facebook_follow();
+            ?>		
+          </div>		  
+</div>
