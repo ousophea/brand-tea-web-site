8559c8b4d85266db46f09ecabbbcc0c9ab29de7b
chundany@gmail.com <chundany@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-01-23 01:56:24 +0000

    slideshow category database

--- Implement/Database/sli_categories.sql
+++ Implement/Database/sli_categories.sql
@@ -10,17 +10,20 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table brand_tea_db.slideshow
-DROP TABLE IF EXISTS `slideshow`;
-CREATE TABLE IF NOT EXISTS `slideshow` (
-  `sli_id` int(10) NOT NULL AUTO_INCREMENT,
-  `sli_image` varchar(300) NOT NULL,
-  `sli_description` tinytext,
-  `sli_cat_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`sli_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Dumping structure for table brand_tea_db.sli_categories
+DROP TABLE IF EXISTS `sli_categories`;
+CREATE TABLE IF NOT EXISTS `sli_categories` (
+  `sli_cat_id` int(10) NOT NULL AUTO_INCREMENT,
+  `sli_cat_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`sli_cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table brand_tea_db.sli_categories: ~2 rows (approximately)
+DELETE FROM `sli_categories`;
+/*!40000 ALTER TABLE `sli_categories` DISABLE KEYS */;
+INSERT INTO `sli_categories` (`sli_cat_id`, `sli_cat_name`) VALUES
+	(1, 'Home');
+/*!40000 ALTER TABLE `sli_categories` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
