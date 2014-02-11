7811b6942f3d4b57d635eba163541ac72a7d41fa
chundany@gmail.com <chundany@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-11 16:53:37 +0000

    side bar right

--- Implement/qnbrandtea.com/application/modules/site/controllers/home.php
+++ Implement/qnbrandtea.com/application/modules/site/controllers/home.php
@@ -5,7 +5,7 @@ class Home extends Base_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model(array('products/mod_product_front', 'content/mod_content'));
+        $this->load->model(array('products/mod_product_front', 'content/mod_content', 'tearelated/mod_tea_front'));
     }
 	
 	public function index()
@@ -16,6 +16,7 @@ class Home extends Base_Controller {
         $config['per_page'] = 9;
         $config['uri_segment'] = 4;
 		$data['products'] = $this->mod_product_front->getProduct($this->uri->segment(4), $config['per_page']);
+        $data['teas'] = $this->mod_tea_front->getTea($this->uri->segment(4), $config['per_page']);
 		
 		$data['title']="Welcome to Tea home page";
 		$data['page']='home';
