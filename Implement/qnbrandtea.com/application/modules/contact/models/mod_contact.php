2732d519b55ccc34b98d1d746bd8245ffd4066fc
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:45:00 +0000

    mod_contact

--- Implement/qnbrandtea.com/application/modules/contact/models/mod_contact.php
+++ Implement/qnbrandtea.com/application/modules/contact/models/mod_contact.php
@@ -1,26 +1,28 @@
 <?php
 
-class mod_contact extends CI_Model { 
-	//View Contact us
+class mod_contact extends CI_Model {
+
+    //View Contact us
     public function getContact() {
-        $this->db->select('*'); 
+        $this->db->select('*');
         $this->db->from(table('contact'));
-		$this->db->join(table('language'), table('contact') . '.' . field('langId') . '=' . table('language') . '.' . field('langId'));
-		$this->db->where(field('lanDes'), $this->lang->line('lang'));
-                
+        $this->db->join(table('language'), table('contact') . '.' . field('langId') . '=' . table('language') . '.' . field('langId'));
+        $this->db->where(field('lanDes'), $this->lang->line('lang'));
         return $this->db->get();
-    }	
-    public function update($conDec,$conStatus,$contactId) {
+    }
+
+    public function update($conDec, $conStatus, $contactId) {
         $data = array(
             field('contactDesc') => $conDec,
-			field('contactStatus')=>$conStatus,
+            field('contactStatus') => $conStatus,
             field('contactId') => $contactId
-            //field('langId') => 2
+                //field('langId') => 2
         );
-        $this->db->where(field('contactId'),$contactId);
-        $result= $this->db->update(table('contact'), $data); 
+        $this->db->where(field('contactId'), $contactId);
+        $result = $this->db->update(table('contact'), $data);
         return $this->db->affected_rows($result);
-    }   
+    }
+
 }
 
 ?>
