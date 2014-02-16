8c4fb8fec3ceec05d0a7b26fdba1c41c1fa6ebc0
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:45:57 +0000

    controller contact

--- trunk/Implement/qnbrandtea.com/application/modules/contact/controllers/contact.php
+++ trunk/Implement/qnbrandtea.com/application/modules/contact/controllers/contact.php
@@ -4,9 +4,9 @@ class Contact extends Admin_Controller {
 
     public function __construct() {
         parent::__construct();
-        //if ($this->checkSession()) {
-        //  redirect('authentication/login');
-        // }
+        if (!$this->checkSession()) {
+          redirect('authentication/login');
+         }
         $this->load->model(array( 'content/mod_content','translation/mod_translation'));
     }
 
@@ -19,8 +19,9 @@ class Contact extends Admin_Controller {
      * By Thary
      */
     public function listcontact() {
+        
+	$data['contact'] = $this->mod_content->getEnContent(3);
         $data['langs'] = $this->mod_translation->getLanguages();
-        $data['contact'] = $this->mod_content->getContent(3);
         $data['title'] = "Contact us";
         $data['page'] = 'listcontact';
         $data['action'] = 'View contact us';
@@ -44,8 +45,62 @@ class Contact extends Admin_Controller {
                 $this->session->set_userdata('ms', $this->lang->line('ms_error'));
             }
         }
-        $data['contact'] = $this->mod_content->getContent(3);
+        $data['contact'] = $this->mod_content->getEnContent(3);
         $this->load->view('masterpage/master', $data);
     }
-
+public function contact_translation($itemId, $lanId) {
+		$this->form_validation->set_rules('txt_name', 'Page Title', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('txt_description', 'Description', 'trim|required');
+        if ($this->form_validation->run() == TRUE) {
+            $conName = $this->input->post('txt_name');
+            $conDes = $this->input->post('txt_description');
+            $action =$this->input->post('action');
+            if($this->mod_content->translate($itemId, $lanId, $conName, $conDes, $action)){
+                $this->session->set_userdata('ms', $this->lang->line('ms_success'));
+                redirect('contact');
+            }
+        }
+        $data['title'] = "Translate";
+        $data['page'] = 'services_translation';
+        $data['action'] = 'Translate';
+        if ($this->input->post('lan_title')) {
+            $data['langTitle'] = $this->input->post('lan_title');
+            $data['itemId']=$this->input->post('item_id');
+            $data['langId']=$this->input->post('lang_id');
+            $data['items']=$this->input->post('item_data');
+        }else{
+            redirect('contact');
+        }
+        
+        $this->load->view('masterpage/master', $data);
+    }
+	//Edit tea related knowledge
+	public function edit() {
+        $data['title'] = "Update Tea Knowledge";
+        $data['page'] = 'edit';
+        $data['action'] = 'Update Tea Knowledge';
+		$check='';
+        if($this->input->post('hid_tea_name')!= $this->input->post('txt_tea_title')){	 
+            $check = '|is_unique['.table('tearelated').'.'. field('teaTitle').']';
+        }
+		$this->form_validation->set_rules('txt_tea_title', 'Tea Related', 'trim|required|max_length[50]'.$check);
+		if ($this->form_validation->run() == TRUE) {
+			$teaName = $this->input->post('txt_tea_title');
+			$teaDec = $this->input->post('txt_tea_dec');
+			$teaStatus = $this->input->post('dd_status');
+		if ($this->mod_tearelated->update($teaName, $teaDec,$teaStatus,$this->uri->segment(3))) {
+				$this->session->set_userdata('ms', 'Success!');
+				redirect('tearelated');
+		}
+        }	
+        $data['tea'] = $this->mod_tearelated->getTea($this->uri->segment(3));
+        $this->load->view('masterpage/master', $data);
+    }
+      public function checkSession() {
+        if ($this->session->userdata('admin')) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
