bbc67d9f36f934c6c5a57da1921ddde2de135a66
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:41:42 +0000

    Tearelated backend

--- trunk/Implement/qnbrandtea.com/application/modules/tearelated/controllers/tearelated.php
+++ trunk/Implement/qnbrandtea.com/application/modules/tearelated/controllers/tearelated.php
@@ -1,17 +1,19 @@
 <?php
+
 class Tearelated extends Admin_Controller {
 
     public function __construct() {
         parent::__construct();
-        //if ($this->checkSession()) {
-          //  redirect('authentication/login');
-       // }
-        $this->load->model('mod_tearelated');
-     }
-    public function index()
-	{
-		$this->listtea();
-	}
+        if (!$this->checkSession()) {
+            redirect('authentication/login');
+        }
+        $this->load->model(array('mod_tearelated', 'translation/mod_translation'));
+    }
+
+    public function index() {
+        $this->listtea();
+    }
+
     /**
      * View all list Tea
      * All Tea related will get from table Tea related Knowledge
@@ -20,7 +22,7 @@ class Tearelated extends Admin_Controller {
         $config['base_url'] = base_url() . $this->uri->segment(1) . '/listtea/';
         $config['total_rows'] = $this->mod_tearelated->getTeaNum();
         $config['per_page'] = 25;
-
+        $data['langs'] = $this->mod_translation->getLanguages();
         $this->pagination->initialize($config);
 
         $data['tearelated'] = $this->mod_tearelated->getAllTea($this->uri->segment(3), $config['per_page']);
@@ -30,63 +32,106 @@ class Tearelated extends Admin_Controller {
         $data['action'] = 'View Tearelated';
         $this->load->view('masterpage/master', $data);
     }
-	 /**
+
+    /**
      * Add New Tea
      * Tea related Knowledge
      */
-	 public function addnewtea() {
+    public function addnewtea() {
         $data['title'] = "New Tea Related Knowledge";
         $data['page'] = 'addnewtea';
         $data['action'] = 'Add New Tea Related Knowledge';
 
         $this->form_validation->set_rules('txt_tea_title', 'Tea Title', 'trim|required|max_length[50]|is_unique[' . table('tearelated') . '.' . field('teaTitle') . ']');
         if ($this->form_validation->run() == TRUE) {
-		    //$teaLang=$this->input->post('tea_lang');
+            //$teaLang=$this->input->post('tea_lang');
             $teaName = $this->input->post('txt_tea_title');
             $teaDec = $this->input->post('txt_tea_dec');
-           // print_r(unserialize($serielizeFields));
+            // print_r(unserialize($serielizeFields));
             //  die();
             if ($this->mod_tearelated->addTea($teaName, $teaDec)) {
                 $this->session->set_userdata('ms', 'Success!');
                 redirect('tearelated');
-           }
+            }
         }
-	    $data['title']="Tea Related Management - Add new";
-		$data['page']='addtea';
-		$data['action']='Add new Tea Related';
+        $data['title'] = "Tea Related Management - Add new";
+        $data['page'] = 'addtea';
+        $data['action'] = 'Add new Tea Related';
         $this->load->view('masterpage/master', $data);
     }
-	//Edit tea related knowledge
-	public function edit() {
+
+    //Edit tea related knowledge
+    public function edit() {
         $data['title'] = "Update Tea Knowledge";
         $data['page'] = 'edit';
         $data['action'] = 'Update Tea Knowledge';
-		$check='';
-        if($this->input->post('hid_tea_name')!= $this->input->post('txt_tea_title')){	 
-            $check = '|is_unique['.table('tearelated').'.'. field('teaTitle').']';
+        $check = '';
+        if ($this->input->post('hid_tea_name') != $this->input->post('txt_tea_title')) {
+            $check = '|is_unique[' . table('tearelated') . '.' . field('teaTitle') . ']';
         }
-		$this->form_validation->set_rules('txt_tea_title', 'Tea Related', 'trim|required|max_length[50]'.$check);
-		if ($this->form_validation->run() == TRUE) {
-			$teaName = $this->input->post('txt_tea_title');
-			$teaDec = $this->input->post('txt_tea_dec');
-			$teaStatus = $this->input->post('dd_status');
-		if ($this->mod_tearelated->update($teaName, $teaDec,$teaStatus,$this->uri->segment(3))) {
-				$this->session->set_userdata('ms', 'Success!');
-				redirect('tearelated');
-		}
-        }	
-        $data['tea'] = $this->mod_tearelated->getTea($this->uri->segment(3));
+        $this->form_validation->set_rules('txt_tea_title', 'Tea Related', 'trim|required|max_length[50]' . $check);
+        if ($this->form_validation->run() == TRUE) {
+            $teaName = $this->input->post('txt_tea_title');
+            $teaDec = $this->input->post('txt_tea_dec');
+            $teaStatus = $this->input->post('dd_status');
+            if ($this->mod_tearelated->update($teaName, $teaDec, $teaStatus, $this->uri->segment(3))) {
+                $this->session->set_userdata('ms', 'Success!');
+                redirect('tearelated');
+            }
+        }
+        $data['teas'] = $this->mod_tearelated->getTea($this->uri->segment(3));
         $this->load->view('masterpage/master', $data);
     }
-	 // Delete tea related knowledge
-	 public function delete(){
-		$id = $this->uri->segment(3);
-		
+
+    // Delete tea related knowledge
+    public function delete() {
+        $id = $this->uri->segment(3);
         if ($this->mod_tearelated->delete($id) > 0) {
             $this->session->set_userdata('ms', 'Success!');
-        }else{
+        } else {
             $this->session->set_userdata('ms', 'Delete fail, please try again!');
-		}
-       redirect('tearelated');
+        }
+        redirect('tearelated');
     }
+
+    public function checkSession() {
+        if ($this->session->userdata('admin')) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+    /**
+     * Translation
+     */
+    public function tea_translation($itemId, $lanId) {
+        $this->form_validation->set_rules('txt_tea_title', 'Tea title', 'trim|required|max_length[100]');
+        if ($this->form_validation->run() == TRUE) {
+            $teaName = $this->input->post('txt_tea_title');
+            $teaDec = $this->input->post('txt_tea_dec');
+            $teaStatus = $this->input->post('dd_status');
+            // $fields = $this->input->post('field');
+            //$labels = $this->input->post('label');
+            // $serielizeFields = serialize(array('label' => $labels, 'field' => $fields));
+            $action = $this->input->post('action');
+            if ($this->mod_tearelated->translate($itemId, $lanId, $teaName, $teaDec, $teaStatus, $action)) {
+                $this->session->set_userdata('ms', $this->lang->line('ms_success'));
+                redirect('tearelated');
+            }
+        }
+        $data['title'] = "Translate";
+        $data['page'] = 'tearelated_translation';
+        $data['action'] = 'Translate';
+        if ($this->input->post('lan_title')) {
+            $data['langTitle'] = $this->input->post('lan_title');
+            $data['tea_id'] = $this->input->post('item_id');
+            $data['langId'] = $this->input->post('lang_id');
+            $data['items'] = $this->input->post('item_data');
+        } else {
+            redirect('tearelated');
+        }
+
+        $this->load->view('masterpage/master', $data);
+    }
+
 }
--- trunk/Implement/qnbrandtea.com/application/modules/tearelated/models/mod_tearelated.php
+++ trunk/Implement/qnbrandtea.com/application/modules/tearelated/models/mod_tearelated.php
@@ -1,13 +1,14 @@
 <?php
 
 class mod_tearelated extends CI_Model {
+
     //Insert new tea
-    public function addTea($teaName,$teaDec) {
-        $data = array(		
-		    field('langId') => 1,
+    public function addTea($teaName, $teaDec) {
+        $data = array(
+            field('langId') => 1,
             field('teaTitle') => $teaName,
             field('teaDesc') => $teaDec,
-            field('teaStatus') => 1 
+            field('teaStatus') => 1
         );
         if ($this->db->insert(table('tearelated'), $data)) {
             return TRUE;
@@ -15,53 +16,102 @@ class mod_tearelated extends CI_Model {
             return FALSE;
         }
     }
-	//list all tea
+
+    //list all tea
     public function getAllTea($start, $num) {
         $this->db->select('*');
         $this->db->limit($num, $start);
         $this->db->from(table('tearelated'));
-		$this->db->join(table('language'), table('tearelated') . '.' . field('langId') . '=' . table('language') . '.' . field('langId'));
-		$this->db->where(field('lanDes'), $this->lang->line('lang'));
-		$this->db->order_by("tea_id", "desc");
+        $this->db->join(table('language'), table('tearelated') . '.' . field('langId') . '=' . table('language') . '.' . field('langId'));
+        $this->db->where(field('lanDes'), $this->lang->line('lang'));
+        $this->db->order_by("tea_id", "desc");
         return $this->db->get();
     }
-	//Get tea to edit
-	public function getTea($tea_id){
-	    $this->db->select('*');
+
+    //Get tea to edit
+    public function getTea($tea_id) {
+        $this->db->select('*');
         $this->db->from(table('tearelated'));
+        $this->db->join(table('language'), table('tearelated') . '.' . field('langId') . '=' . table('language') . '.' . field('langId'));
+        $this->db->where(field('lanDes'), $this->lang->line('lang'));
         $this->db->where(field('teaId'), $tea_id);
         return $this->db->get();
-	}
-	//update tea related knowledge
-	
-    public function update($teaName, $teaDec,$teaStatus, $teaId) {
+    }
+
+    /*
+     * Translation
+     */
+
+    public function checkLang($teaId, $langId) {
+        $this->db->select('*');
+        $this->db->from(table('teaLang'));
+        $this->db->where(field('teaId'), $teaId);
+        $this->db->where(field('langId'), $langId);
+        return $this->db->get();
+    }
+
+    public function translate($itemId, $lanId, $teaName, $teaDec, $teastatus, $action) {
+        $data = array(
+            field('teaId') => $itemId,
+            field('langId') => $lanId,
+            field('teaTitle') => $teaName,
+            field('teaDesc') => $teaDec,
+            field('teaStatus') => $teastatus
+        );
+        if ($action == 'insert') {
+            return $this->db->insert(table('teaLang'), $data);
+        } else if ($action == 'update') {
+            $this->db->where(field('teaId'), $itemId);
+            $this->db->where(field('langId'), $lanId);
+            return $this->db->update(table('teaLang'), $data);
+        }
+        return FALSE;
+    }
+
+    //update tea related knowledge
+
+    public function update($teaName, $teaDec, $teaStatus, $teaId) {
         $data = array(
             field('teaTitle') => $teaName,
             field('teaDesc') => $teaDec,
-			field('teaStatus')=>$teaStatus,
+            field('teaStatus') => $teaStatus,
             field('teaId') => $teaId
-            //field('langId') => 2
+                //field('langId') => 2
         );
-        $this->db->where(field('teaId'),$teaId);
-        $result= $this->db->update(table('tearelated'), $data); 
+        $this->db->where(field('teaId'), $teaId);
+        $result = $this->db->update(table('tearelated'), $data);
         return $this->db->affected_rows($result);
     }
-	//Delete tea knowledge
+
+    //Delete tea knowledge
     public function delete($id) {
         $this->db->where(field('teaId'), $id);
-        $result = $this->db->delete(table('tearelated'));
-		return $this->db->affected_rows($result);
+        if ($this->db->delete(table('tearelated'))) {
+            $this->delete_tea_lan($id);
+            return TRUE;
+        }
+        return FALSE;
     }
-    
-	public function getTeaNum(){
+
+    public function delete_tea_lan($id) {
+        $this->db->where(field('teaId'), $id);
+        if ($this->db->delete(table('teaLang'))) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function getTeaNum() {
         $objDb = $this->getAllt();
         return $objDb->num_rows();
-    } 
-	 public function getAllt() {
+    }
+
+    public function getAllt() {
         $this->db->select('*');
         $this->db->from(table('tearelated'));
         return $this->db->get();
     }
+
 }
 
 ?>
