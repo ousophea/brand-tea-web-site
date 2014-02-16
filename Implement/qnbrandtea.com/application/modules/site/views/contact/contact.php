8554a0cca3cb438377b7dc4d881230b9506a4a5a
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:49:30 +0000

    contact for site

--- Implement/qnbrandtea.com/application/modules/site/views/contact/contact.php
+++ Implement/qnbrandtea.com/application/modules/site/views/contact/contact.php
@@ -5,16 +5,30 @@
     <?php
     echo br(1);
     foreach ($contact->result_array() as $con) {
-        ?>   
-        <div class="row-fluid span12">				
-            <?php
-            echo $con[field('conDes')];
-            ?>
-        </div>
-            <?php
-        }
-        echo br(1);
         ?> 
-
-
+        <div class="row-fluid">
+            <div class="span12">				
+                <?php
+                echo "<ul>".$con[field('conDes')]."</ul>";
+                ?>
+            </div>
+        </div>
+        <?php
+    }
+    echo br(1);
+    ?> 
+     <div class="row-fluid ">
+        <h3 class="page-head"><?php echo $this->lang->line('men_skype'); ?></h3>  
+            <?php
+            echo skype_helper();
+            ?>
+    </div>
+    <div class="row-fluid ">
+        <h3 class="page-head"><?php echo $this->lang->line('men_location'); ?></h3>
+        <div class='span1'>
+            <?php
+            echo map_helper();
+            ?>
+        </div>	
+    </div>
 </div>
