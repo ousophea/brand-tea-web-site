c250dded80ebdc063aaf6fe5ca818c3f7afa824f
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:54:16 +0000

    controller about

--- trunk/Implement/qnbrandtea.com/application/modules/site/controllers/about.php
+++ trunk/Implement/qnbrandtea.com/application/modules/site/controllers/about.php
@@ -7,10 +7,9 @@ class About extends Base_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('about/mod_about_front');
+        $this->load->model(array( 'content/mod_content','translation/mod_translation'));
         $this->load->helper('facebook');
         // Load language
-        $this->load->helper('checklang');
     }
 
     public function index() {
@@ -18,12 +17,11 @@ class About extends Base_Controller {
     }
 
     public function about() {
-        $data['slideshow'] = $this->mod_slideshow->getSlideshowByCatId(2);
-        $data['about'] = $this->mod_about_front->getAbout();
-
-        $data['title'] = "About us";
+        $data['slideshow'] = $this->mod_slideshow->getSlideshowByCatId(2);   
+        $data['about'] = $this->mod_content->getContent(2);
+        $data['title'] = $this->lang->line('men_about');
         $data['page'] = 'about/about';
-        $data['action'] = 'About us';
+        $data['action'] = $this->lang->line('men_about');
         $this->load->view('master', $data);
     }
 
