33eb98b7b2e3687b991a483930ef075c08c1c7c0
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:53:55 +0000

    controller contact

--- Implement/qnbrandtea.com/application/modules/site/controllers/contact.php
+++ Implement/qnbrandtea.com/application/modules/site/controllers/contact.php
@@ -4,27 +4,23 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Contact extends Base_Controller {
-
     function __construct() {
         parent::__construct();
         $this->load->model(array('contact/mod_contact_front','content/mod_content'));
-        //$this->load->helper('facebook');
+        $this->load->helper('facebook');
+        $this->load->helper('map');
+        $this->load->helper('skype');
         // Load language
-        $this->load->helper('checklang');
     }
-
     public function index() {
         $this->contact();
     }
-
     public function contact() {
         $data['slideshow'] = $this->mod_slideshow->getSlideshowByCatId(6);
         $data['contact'] = $this->mod_content->getContent(3);
-
-        $data['title'] = "Contact us";
+        $data['title'] = $this->lang->line('men_contact');
         $data['page'] = 'contact/contact';
-        $data['action'] = 'contact us';
+        $data['action'] = $this->lang->line('men_contact');
         $this->load->view('master', $data);
     }
-
 }
