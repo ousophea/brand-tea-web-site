86e5f578fdd0285282eaa72853781dfe81dbfb25
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:43:10 +0000

    contact_translation backend

---
+++ Implement/qnbrandtea.com/application/modules/contact/views/contact_translation.php
@@ -0,0 +1,53 @@
+<?php 
+echo form_open('contact/contact_translation/' . $itemId . '/' . $langId);
+echo form_hidden('lan_title', $langTitle);
+echo form_hidden('lang_id',$langId);
+?>
+<div class="holder">
+    <div class="sub-title"><h2>Translate to <?php echo $langTitle; ?></h2>
+         <?php
+            echo validation_errors();
+        ?>
+    </div>
+
+    <div class="action">
+        <?php echo form_submit('save', $this->lang->line('save'), 'class="add"'); ?>
+        <?php echo anchor('contact', $this->lang->line('cancel'), 'class="button cancel"'); ?>
+    </div>
+    <div style="clear:both"></div>
+</div>
+<?php
+$services = unserialize($items);
+$action ='insert';
+// Check existing data
+$this->load->model('content/mod_content');
+$langData = $this->mod_content->checkLang($itemId, $langId);
+if($langData->num_rows()>0){
+    $services = $langData->result_array();
+    $action='update';
+}
+echo form_hidden('action', $action);
+foreach ($services as $service):
+    if ($service[field('conId')] == $itemId):
+?>
+<div class="holder">
+    <h3><?php echo $this->lang->line('title'); ?>: <input type="text" name="txt_name" placeholder="Page Title" value="<?php echo $service[field('conName')]; ?>" required /> <?php echo $this->lang->line('require'); ?></h3>
+    <p><textarea name="txt_description" rows="10" required ><?php echo $service[field('conDes')]; ?></textarea></p>
+    <br>
+</div>
+<?php 
+	endif;
+endforeach;
+?>
+<?php echo form_close(); ?>
+<script type="text/javascript">
+tinymce.init({
+    selector: "textarea",
+    plugins: [
+        "advlist autolink lists link image charmap preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
+</script>
