2d96b0694685151503cf502cba42115fb48345a6
thary.sat@gmail.com <thary.sat@gmail.com@7131e8a6-bf0b-4387-cb28-7e58ed7d3f5c>
2014-02-16 15:49:56 +0000

    about for site

--- Implement/qnbrandtea.com/application/modules/site/views/about/about.php
+++ Implement/qnbrandtea.com/application/modules/site/views/about/about.php
@@ -1,28 +1,24 @@
 <div style="height: 10px;"></div>
 <div class="row-fluid">
-    
-        <h3 class="page-head"><?php echo $action; ?></h3>    
+    <h3 class="page-head"><?php echo $action; ?></h3>    
+    <?php
+    echo br(1);
+    foreach ($about->result_array() as $abo) {
+        ?>     				
+        <?php
+        echo "<div class='row-fluid'><div class='span12'><ul>";
+        echo $abo[field('conDes')];
+        echo "</ul></div></div>";
+        ?>
+        <?php
+    }
+    ?> 
+    <div class="row-fluid ">
+        <h3 class="page-head"><?php echo $this->lang->line('find_us'); ?></h3>
+        <div class='span1'>
             <?php
-            echo br(1);
-            foreach ($about->result_array() as $abo) {
-                ?>   
-				  <div class="row-fluid span12">				
-                    <?php
-                     
-				     echo $abo[field('conDes')];
-                   
-                    ?>
-                </div>
-                <?php
-            }
-			echo br(1);
-            ?> 
-	       
-          <div class="row-fluid span12">	
-		   <h3 class="page-head"><?php echo $this->lang->line('men_follow'); ?></h3>
-		   <?php
-		   echo br(1);
-            echo facebook_follow();
-            ?>		
-          </div>		  
+            echo facebook_like();
+            ?>
+        </div>	
+    </div>
 </div>
